b28bf1fe4e6f1069225fb587604174010b609b28
Suprim <suprim@heubert.com>
2021-10-14 09:43:42 +0545

    feat: sw updates

---
+++ src/service-worker.js
@@ -0,0 +1,97 @@
+/// <reference lib="webworker" />
+/* eslint-disable no-restricted-globals */
+import { clientsClaim } from "workbox-core";
+import { ExpirationPlugin } from "workbox-expiration";
+import { precacheAndRoute, createHandlerBoundToURL } from "workbox-precaching";
+import { registerRoute } from "workbox-routing";
+import { StaleWhileRevalidate, CacheFirst } from "workbox-strategies";
+import { CacheableResponsePlugin } from "workbox-cacheable-response";
+
+clientsClaim();
+
+self.skipWaiting();
+
+precacheAndRoute(self.__WB_MANIFEST);
+
+const fileExtensionRegexp = new RegExp("/[^/?]+\\.[^/]+$");
+
+registerRoute(({ request, url }) => {
+  if (request.mode !== "navigate") {
+    return false;
+  }
+
+  if (url.pathname.startsWith("/_")) {
+    return false;
+  }
+
+  if (url.pathname.match(fileExtensionRegexp)) {
+    return false;
+  }
+
+  return true;
+}, createHandlerBoundToURL(process.env.PUBLIC_URL + "/index.html"));
+
+registerRoute(
+  ({ url }) => url.origin === "http://fonts.googleapis.com",
+  new StaleWhileRevalidate({
+    cacheName: "google-fonts-stylesheet",
+  })
+);
+
+registerRoute(
+  ({ url }) => url.origin === "https://fonts.gstatic.com",
+  new CacheFirst({
+    cacheName: "google-fonts-webfonts",
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxEntries: 30,
+        maxAgeSeconds: 30 * 24 * 60 * 60,
+      }),
+    ],
+  })
+);
+
+registerRoute(
+  ({ url }) =>
+    url.origin === self.location.origin && url.pathname.endsWith(".png"),
+  new StaleWhileRevalidate({
+    cacheName: "images",
+    plugins: [new ExpirationPlugin({ maxEntries: 50 })],
+  })
+);
+
+registerRoute(
+  ({ request }) => request.destination === "image",
+  new CacheFirst({
+    cacheName: "static-images",
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxEntries: 30,
+        maxAgeSeconds: 30 * 24 * 60 * 60,
+      }),
+    ],
+  })
+);
+
+registerRoute(
+  ({ request }) =>
+    request.destination === "script" || request.destination === "style",
+  new StaleWhileRevalidate({
+    cacheName: "static-resources",
+    plugins: [new ExpirationPlugin({ maxEntries: 50 })],
+  })
+);
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
+});
+
+// Any other custom service worker logic can go here.
